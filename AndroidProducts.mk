8b4c1171a08cc359018ea8285c8e45b2dd709cd4
zeeshan <itexpert120@gmail.com>
2021-01-27 20:18:49 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,8 @@
 LOCAL_PATH := device/samsung/serranovelte
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_serranovelte.mk \
-	$(LOCAL_DIR)/pa_serranovelte.mk \
 	$(LOCAL_DIR)/lineage_serranovelte.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_serranovelte-eng \
+    lineage_serranovelte-userdebug \
+    lineage_serranovelte-user
